0ea2e3bdfb00abf591d227da082f9217e391b900
umerwaseem99 <muhammad.15217@iqra.edu.pk>
2021-05-25 03:16:38 +0500

    'madproject'

---
+++ app/src/main/java/com/example/music/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.music;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int SPLASH_TIME_OUT = 5000;
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        ImageView imageView= (ImageView)findViewById(R.id.imageView);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),musiclist.class);
+                startActivity(i);
+                finish();
+
+            }
+        },SPLASH_TIME_OUT);
+
+    }
+}
