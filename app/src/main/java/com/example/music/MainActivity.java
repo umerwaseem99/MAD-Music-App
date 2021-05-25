947d86fcd20fff42d3c3a71f83670ebaa14dcc12
MuhammadAnas190 <mohammad.anas190@gmail.com>
2021-05-25 05:53:47 +0500

    fix: Splash screen center > storage permission

--- app/src/main/java/com/example/music/MainActivity.java
+++ app/src/main/java/com/example/music/MainActivity.java
@@ -2,11 +2,19 @@ package com.example.music;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 import android.widget.ImageView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,12 +30,10 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(getApplicationContext(),musiclist.class);
+                Intent i = new Intent(getApplicationContext(), MusicList.class);
                 startActivity(i);
                 finish();
-
             }
         },SPLASH_TIME_OUT);
-
     }
 }
